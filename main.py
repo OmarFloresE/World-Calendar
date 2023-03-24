2dbaa71b1c2fca3cc31b0b77c983d6bdc4849df3
Omar Flores Estrada <43051706+OmarFloresE@users.noreply.github.com>
2023-03-24 16:55:22 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,63 @@
-output = "Hello"
+# clear()	Removes all the elements from the dictionary
+# copy()	Returns a copy of the dictionary
+# fromkeys()	Returns a dictionary with the specified keys and value
+# get()	Returns the value of the specified key
+# items()	Returns a list containing a tuple for each key value pair
+# keys()	Returns a list containing the dictionary's keys
+# pop()	Removes the element with the specified key
+# popitem()	Removes the last inserted key-value pair
+# setdefault()	Returns the value of the specified key. If the key does not exist: insert the key, with the specified value
+# update()	Updates the dictionary with the specified key-value pairs
+# values()	Returns a list of all the values in the dictionary
 
-print(output)
+###############################################################################################################################
+
+# myfamily = {
+#   "child1" : {
+#     "name" : "Emil",
+#     "year" : 2004
+#   },
+#   "child2" : {
+#     "name" : "Tobias",
+#     "year" : 2007
+#   },
+#   "child3" : {
+#     "name" : "Linus",
+#     "year" : 2011
+#   }
+# } 
+
+# child1 = {
+#   "name" : "Emil",
+#   "year" : 2004
+# }
+# child2 = {
+#   "name" : "Tobias",
+#   "year" : 2007
+# }
+# child3 = {
+#   "name" : "Linus",
+#   "year" : 2011
+# }
+
+# myfamily = {
+#   "child1" : child1,
+#   "child2" : child2,
+#   "child3" : child3
+# } 
+
+# How to access items on a nested dictionary: 
+# print(myfamily["child2"]["name"]) 
+
+# Thought:
+# What if we make a file that has all the notable holidays and we match them with occurence on a set date??
+
+
+thisHoliday = {
+    "holiday": "Christmas",
+    "date": "12/24/2023",
+    "link": "https://en.wikipedia.org/wiki/Christmas"
+}
+
+x = thisHoliday.items()
+print(x)
